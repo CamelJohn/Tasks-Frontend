947ff6a1792d32310ee9e2d5a79d6c7e4dda5ef8
Jonathan A <jatia-c@myhippo.com>
2022-01-21 15:04:23 +0200

    experimenting with color pallette

--- src/Components/Sidenav/index.tsx
+++ src/Components/Sidenav/index.tsx
@@ -3,9 +3,9 @@ import css from './styles.module.scss';
 const Sidenav: React.FC<{}> = () => {
     return <nav className={css.container}>
         <ul className={css.links}>
-            <li className={css.link}>Tasks</li>
-            <li className={css.link}>Categories</li>
-            <li className={css.link}>Dashboard</li>
+            <li className={css.navlink}>Tasks</li>
+            <li className={css.navlink}>Categories</li>
+            <li className={css.navlink}>Dashboard</li>
         </ul>
     </nav>
 }
