ab6affd6da48214ea18a7880f378f8fe09b1252f
Jonathan A <jatia-c@myhippo.com>
2022-01-23 21:59:02 +0200

    navigate on logo click

--- src/Components/Logo/index.tsx
+++ src/Components/Logo/index.tsx
@@ -1,7 +1,9 @@
 import css from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Logo: React.FC<{ logo: string }> = ({ logo }) => {
-    return <div className={css.logo}>{logo}</div>
+    const navigate = useNavigate();
+    return <div onClick={() => navigate('/')} className={css.logo}>{logo}</div>
 }
 
 export default Logo;
